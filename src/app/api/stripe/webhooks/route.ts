1e380b72ad9e5c9b3b6270ab471df778e1b3f201
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-09-16 19:54:13 +0200

    show purchased products

--- src/app/api/stripe/webhooks/route.ts
+++ src/app/api/stripe/webhooks/route.ts
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
-import { Cart } from "@/models/Cart";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2023-08-16",
