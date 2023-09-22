2998d1074cbf2fe019ee637fc6b29c586795d0dd
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-09-22 15:30:32 +0200

    wishlists link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@
 
 import Link from 'next/link';
 import React from 'react';
-import { HiOutlineSearch } from 'react-icons/hi';
 import '../styles/header.css';
 import { useSession } from "next-auth/react";
 import { useCart } from '@/hooks/CartContext';
+import { FaHeart } from 'react-icons/fa';
 
 export const Navbar = () => {
   const { data: session, status } = useSession();
-  const { cartItems } = useCart();
+  const { cartItems, userCart } = useCart();
 
   const totalQuantity = cartItems.reduce((total, cartItem) => {
     return total + cartItem.quantity;
@@ -23,13 +23,14 @@ export const Navbar = () => {
         <li><Link href="/sudaderas">Sudaderas</Link></li>
       </ul>
       <ul>
-        <li><div className='search'><HiOutlineSearch /></div></li>
         {
           status === "authenticated" ?
             <li><Link href="/user/account/profile">{session.user.fullname.split(' ')[0]}</Link></li>
             :
             <li><Link href="/login">Login</Link></li>
         }
+        <li><Link href="/user/wishlists">Wishlists ({userCart ? userCart?.favorites.length : 0})</Link></li>
+
         <li><Link href="/user/cart">Cesta ({totalQuantity})</Link></li>
       </ul>
     </header>
