d4bb4ff7969a53c4f72da52844dc187862eac569
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-09 23:13:24 +0200

    url

--- src/helpers/getProducts.js
+++ src/helpers/getProducts.js
@@ -1,5 +1,7 @@
 export async function getProducts(key = "") {
-    const res = await fetch(`http://localhost:3000/api/products?${key}`)
+    const { NEXT_URL } = process.env;
+    
+    const res = await fetch(`${NEXT_URL}/api/products?${key}`)
 
     if (!res.ok) {
         throw new Error('Failed to fetch data')
