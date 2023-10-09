5863d7a1d39b8b37ba2bc25fb2e5aa09aaa7de4a
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-10 01:14:32 +0200

    more bugs for production

--- src/app/api/stripe/payment/route.ts
+++ src/app/api/stripe/payment/route.ts
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2023-08-16"
 });
 
-export async function loadPrices() {
+async function loadPrices() {
     const prices = await stripe.prices.list();
     return prices.data;
 }
