2298ef4adaec86a68a8ccf3464376c335eb7af66
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-10 19:42:59 +0200

    error handling in product fetching

--- src/helpers/ordersFunctions.js
+++ src/helpers/ordersFunctions.js
@@ -82,7 +82,8 @@ export const getOrdersWithProducts = async (userId) => {
                 const res = await fetch(`/api/products?_id=${productId}`);
 
                 if (!res.ok) {
-                    throw new Error('Failed to fetch data')
+                    const errorData = await res.json();
+                    throw new Error(`Failed to fetch data. Status: ${res.status}, Message: ${errorData.message}`);
                 }
 
                 return res.json();
