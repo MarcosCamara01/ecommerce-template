8451330558c239753731d8d910583e5fb3fedd49
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-13 01:07:26 +0200

    fixing error unexpected token

--- src/helpers/getProducts.js
+++ src/helpers/getProducts.js
@@ -1,14 +1,21 @@
 export async function getProducts(key = "") {
     const { NEXT_URL } = process.env;
 
-    const res = await fetch(`${NEXT_URL}/api/products${key}`)
+    try {
+        const res = await fetch(`${NEXT_URL}/api/products${key}`)
 
-    if (!res.ok) {
-        const errorData = await res.json();
-        throw new Error(`Failed to fetch data. Status: ${res.status}, Message: ${errorData.message}`);
+        if (!res.ok) {
+            const errorData = await res.json();
+            throw new Error(`Failed to fetch data. Status: ${res.status}, Message: ${errorData.message}`);
+        }
+
+        const jsonData = await res.json();
+
+        return jsonData;
+
+    } catch (error) {
+        console.error('Error fetching cart:', error);
     }
 
-    const jsonData = await res.json();
-
-    return jsonData;
+    return null;
 }
