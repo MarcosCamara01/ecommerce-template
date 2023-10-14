7f05934b0847463869c22edc3eccfc77ba45f37f
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-14 02:06:36 +0200

    trying to resolve the error in the deployment 01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,25 @@ import { getProducts } from "@/helpers/getProducts";
 import { Products } from "../components/Products";
 
 const Home = async () => {
-  let products = await getProducts();
-  if (products) {
-    products.reverse();
-  }
+  try {
+    let products = await getProducts();
+    if (products) {
+      products.reverse();
+    }
 
-  return (
-    <section className="section-products">
-      <Products
-        products={products}
-      />
-    </section>
-  );
+    return (
+      <section className="section-products">
+        <Products
+          products={products}
+        />
+      </section>
+    );
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return (
+      <div>Error fetching products. Please try again later.</div>
+    );
+  }
 }
 
 export default Home;
