06551779f431823ec5ed8ed577b152a582340c7f
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-14 15:17:24 +0200

    deployment error 02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-import { getProducts } from "@/helpers/getProducts";
-import { Products } from "../components/Products";
-
 const Home = async () => {
-  try {
-    let products = await getProducts();
-    if (products) {
-      products.reverse();
-    }
-
-    return (
-      <section className="section-products">
-        <Products
-          products={products}
-        />
-      </section>
-    );
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    return (
-      <div>Error fetching products. Please try again later.</div>
-    );
-  }
+  return (
+    <h1>Home</h1>
+  )
 }
 
 export default Home;
