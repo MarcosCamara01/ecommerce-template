4183de0de14ce912d4f4f83f08b69d3f3ac1f059
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-16 15:52:41 +0200

    products route update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ const Home = async () => {
   let products = [];
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/allProducts`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/products`)
     if (!res.ok) {
       const errorData = await res.json();
       throw new Error(`Failed to fetch data. Status: ${res.status}, Message: ${errorData.message}`);
     }
 
     products = await res.json();
+    products.reverse();
 
   } catch (error) {
     console.error('Error fetching products:', error);
