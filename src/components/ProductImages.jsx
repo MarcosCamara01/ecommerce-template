49d3f27640c3387bd5f032bd9fe79475837b2435
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-19 19:15:28 +0200

    show more show less desktop functionality

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,18 +1,39 @@
 'use client'
 
-import React from 'react'
+import React, { useState, useRef } from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination, Scrollbar, Zoom } from 'swiper/modules'
 import { useClientMediaQuery } from '@/hooks/useClientMediaQuery'
 import { Loader } from '@/helpers/Loader'
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { MdKeyboardArrowUp } from 'react-icons/md';
 
 import 'swiper/css'
 import "swiper/css/zoom";
 import 'swiper/css/pagination'
 
 export const ProductImages = ({ images, name }) => {
+  const initialImagesToShow = 4;
+  const [visibleImages, setVisibleImages] = useState(initialImagesToShow);
+  const scrollRef = useRef(null);
+
   const isMobile = useClientMediaQuery('(max-width: 680px)');
+
+  const showImages = () => {
+    if (visibleImages < images.length) {
+      setVisibleImages(images.length);
+    } else {
+      setVisibleImages(initialImagesToShow);
+      scrollToButton();
+    }
+  };
+
+  const scrollToButton = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+  };
 
   if (isMobile === null) {
     return <Loader />
@@ -20,7 +41,7 @@ export const ProductImages = ({ images, name }) => {
 
   return (
     <>
-      {isMobile ?
+      {isMobile ? (
         <div className='container'>
           <Swiper
             modules={[Zoom, Pagination, Scrollbar]}
@@ -30,29 +51,44 @@ export const ProductImages = ({ images, name }) => {
             zoom={true}
           >
             {images.map((image, index) => (
-              <SwiperSlide key={index}> 
-                  <img
-                    src={image}
-                    alt={`${name} - Image ${index + 1}`}
-                    className="product-img"
-                  />
+              <SwiperSlide key={index}>
+                <img
+                  src={image}
+                  alt={`${name} - Image ${index + 1}`}
+                  className='product-img'
+                />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
-        :
-        <div className='bx-grid'>
-          {images.map((image, index) => (
-            <div key={index}>
+      ) : (
+        <div className='bx-grid' ref={scrollRef}>
+          {images.slice(0, visibleImages).map((image, index) => (
+            <div className='bx-image' key={index} >
               <img
                 src={image}
                 alt={`${name} - Image ${index + 1}`}
-                className="product-img"
+                className='product-img'
               />
             </div>
           ))}
+          {
+            images.length > 4 &&
+            (
+              <div className='expand-button'>
+                <button
+                  onClick={showImages}
+                  className={visibleImages < images.length ? "" : "transform"}>
+                  {visibleImages < images.length
+                    ? "SHOW MORE"
+                    : "SHOW LESS"}
+                  <MdKeyboardArrowDown />
+                </button>
+              </div>
+            )
+          }
         </div>
-      }
+      )}
     </>
-  )
+  );
 }
