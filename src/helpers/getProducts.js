fb184742944bc6c236185361b5d1881a17ae5cac
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-21 23:20:57 +0200

    images bug

--- src/helpers/getProducts.js
+++ src/helpers/getProducts.js
@@ -1,6 +1,6 @@
 export async function getProducts(key = "") {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/products${key}`)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/products${key}`, {cache: "no-store"});
         if (!res.ok) {
             const errorData = await res.json();
             throw new Error(`Failed to fetch data. Status: ${res.status}, Message: ${errorData.message}`);
