8fb55e7d90b4d7dee41406d70a698ebd8f3387f9
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-10-22 19:03:38 +0200

    orderscontext functionality

--- src/app/(user)/account/orders/[id]/page.jsx
+++ src/app/(user)/account/orders/[id]/page.jsx
@@ -3,36 +3,53 @@
 import { useEffect, useState } from 'react';
 import { useSession } from "next-auth/react";
 import { Products } from '@/components/Products';
-import '@/styles/orders.css';
 import { useParams } from 'next/navigation';
 import { Loader } from "@/helpers/Loader";
 import { format } from 'date-fns';
-import { getOrdersWithProducts } from '@/helpers/ordersFunctions';
+import { getOrders } from '@/helpers/ordersFunctions';
+import { useOrders } from '@/hooks/OrdersContext';
+
+import '@/styles/orders.css';
 
 const OrderDetails = () => {
     const params = useParams();
-    const { data: session, status } = useSession();
-    const [userOrder, setUserOrder] = useState(null);
+    const { data: session } = useSession();
+    const { orders, setOrders } = useOrders();
+    const [order, setOrder] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const orderId = params.id;
 
     useEffect(() => {
-        if (status === "authenticated" && orderId) {
+        if (orderId && orders === null) {
             const fetchUserOrders = async () => {
                 try {
                     const userId = session.user._id;
-                    const ordersWithProducts = await getOrdersWithProducts(userId);
-                    const order = ordersWithProducts.orders.find((order) => order._id === orderId);
-                    setUserOrder(order);
+                    const response = await getOrders(userId);
+                    if (response && Array.isArray(response.orders)) {
+                        const order = response.orders.find((order) => order._id === orderId);
+                        setOrder(order);
+
+                        response.orders.sort((a, b) => new Date(b.purchaseDate) - new Date(a.purchaseDate));
+                        setOrders(response.orders);
+                    } else {
+                        console.log("No orders available.");
+                    }
+
                     setLoading(false);
                 } catch (error) {
                     console.error("Error fetching orders:", error);
                 }
             };
+
             fetchUserOrders();
+        } else if (orderId && orders !== null) {
+            const order = orders.find((order) => order._id === orderId);
+            setOrder(order);
+            setLoading(false);
         }
-    }, [status, orderId]);
+
+    }, [orderId]);
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
@@ -40,51 +57,51 @@ const OrderDetails = () => {
     };
 
     const renderOrderDetails = () => {
-        if (userOrder) {
-            const totalProducts = userOrder.products.reduce((total, product) => total + product.quantity, 0);
+        if (order) {
+            const totalProducts = order.products.reduce((total, product) => total + product.quantity, 0);
 
             const productsText = totalProducts === 1 ? "item" : "items";
 
             return (
                 <div className="order-container">
                     <div className='order-products'>
-                        <Products products={userOrder.products} />
+                        <Products products={order.products} />
                     </div>
                     <div className='order-details'>
                         <div className='details'>
                             <h3>ORDER DETAILS</h3>
-                            <div className='bx-info'><span>Order Number</span> <span>{userOrder?.orderNumber}</span></div>
-                            <div className='bx-info'><span>Order Date</span> <span>{formatDate(userOrder.purchaseDate)}</span></div>
-                            <div className='bx-info'><span>Expected Delivery Date</span> <span>{formatDate(userOrder.expectedDeliveryDate)}</span></div>
+                            <div className='bx-info'><span>Order Number</span> <span>{order?.orderNumber}</span></div>
+                            <div className='bx-info'><span>Order Date</span> <span>{formatDate(order.purchaseDate)}</span></div>
+                            <div className='bx-info'><span>Expected Delivery Date</span> <span>{formatDate(order.expectedDeliveryDate)}</span></div>
                         </div>
                         <div className='details'>
                             <h3>DELIVERY ADDRESS</h3>
                             <ul>
-                                <li>{userOrder.name}</li>
-                                <li>{userOrder.address.line1}</li>
-                                {userOrder.address.line2 && (
-                                    <li>{userOrder.address.line2}</li>
+                                <li>{order.name}</li>
+                                <li>{order.address.line1}</li>
+                                {order.address.line2 && (
+                                    <li>{order.address.line2}</li>
                                 )}
-                                <li>{userOrder.address.postal_code} {userOrder.address.city}</li>
-                                {userOrder.phone && (
-                                    <li>{userOrder.phone}</li>
+                                <li>{order.address.postal_code} {order.address.city}</li>
+                                {order.phone && (
+                                    <li>{order.phone}</li>
                                 )}
-                                <li>{userOrder.email}</li>
+                                <li>{order.email}</li>
                             </ul>
                         </div>
                         <div className='details'>
                             <h3>TOTALS</h3>
-                            <div className='bx-info'><span>{totalProducts} {productsText}</span> <span>{(userOrder.total_price / 100).toFixed(2)} €</span></div>
+                            <div className='bx-info'><span>{totalProducts} {productsText}</span> <span>{(order.total_price / 100).toFixed(2)} €</span></div>
                             <div className='bx-info'><span>Delivery</span> <span>FREE</span></div>
-                            <div className='bx-info'><span>Total Discount</span> <span>{userOrder.discount ? userOrder.discount : 0} €</span></div>
-                            <div className='bx-info'><span>Total</span> <span>{(userOrder.total_price / 100).toFixed(2)} €</span></div>
+                            <div className='bx-info'><span>Total Discount</span> <span>{order.discount ? order.discount : 0} €</span></div>
+                            <div className='bx-info'><span>Total</span> <span>{(order.total_price / 100).toFixed(2)} €</span></div>
                             <div className='bx-info'>(VAT included)</div>
                         </div>
                     </div>
                 </div>
             );
         } else {
-            return <h4>Order not found.</h4>;
+            return <p>Order not found.</p>;
         }
     };
 
