82193631137f19e395cccadf87ed51ed003d74b5
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-11-05 18:46:16 +0100

    Product colors

---
+++ src/helpers/colorMapping.js
@@ -0,0 +1,9 @@
+export const colorMapping = {
+    'Black': '#000',
+    'White': '#fff',    
+    'Blue': 'rgb(67, 77, 88)',
+    'Mid-blue': 'rgb(77, 107, 135)',
+    'Dark-brown': 'rgb(71, 60, 52)',
+    'Light-blue': 'rgb(170, 182, 187)',
+    'Grey': '#808080'
+};
