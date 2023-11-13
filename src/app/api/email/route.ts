ee5f8580232490b62b3cf0ae8d24eb294373b25d
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-11-13 12:24:18 +0100

    trying to fix bug in sendEmail

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -4,13 +4,20 @@ const nodemailer = require('nodemailer');
 export async function POST(request: NextRequest) {
     const { name, email, message, subject } = await request.json();
 
+    if (!name || !email || !message || !subject) {
+        return NextResponse.json({ message: "We need more information to send an email!" }, { status: 400 })
+    }
+
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
 
             user: process.env.NEXT_PUBLIC_EMAIL_USERNAME,
             pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD
-        }
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
     });
 
     const mailOptions = {
@@ -25,20 +32,12 @@ export async function POST(request: NextRequest) {
     }
 
     try {
-        await new Promise((resolve, reject) => {
-            transporter.sendMail(mailOptions, (err: any, response: any) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(response);
-                }
-            });
-        });
-
-        return NextResponse.json({ message: "Success: email was sent" }, { status: 200 })
-
-    } catch (error) {
-        console.log(error)
-        NextResponse.json({ message: "COULD NOT SEND MESSAGE" }, { status: 500 })
+        await transporter.sendMail(mailOptions);
+        return NextResponse.json(
+            { message: "Email sent successfully!" },
+            { status: 200 },
+        );
+    } catch (error: any) {
+        return NextResponse.json({ message: error.toString() }, { status: 500 });
     }
 }
