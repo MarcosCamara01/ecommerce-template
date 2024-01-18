ee3ba5c1836efcb1c61d1ab3109f01c147a00f88
Marcos Penelas <marcospenelascamara@gmail.com>
2024-01-18 13:17:49 +0000

    result page is a ts file

--- src/app/(user)/result/page.tsx
+++ src/app/(user)/result/page.tsx
@@ -7,13 +7,14 @@ import { useCart } from '@/hooks/CartContext';
 import { saveOrder } from "@/helpers/ordersFunctions";
 import { Loader } from "@/helpers/Loader";
 import { useSession } from 'next-auth/react';
-import { sendEmail } from "@/helpers/sendEmail"
+import { sendEmail } from "@/helpers/sendEmail";
+import Stripe from 'stripe';
 
 const CheckoutSuccess = () => {
   const searchParams = useSearchParams();
   const { data: session, status } = useSession();
   const { cartItems, setCartItems } = useCart();
-  const [data, setData] = useState();
+  const [data, setData] = useState<Stripe.Checkout.Session | { error: boolean, errorMessage: string }>();
   const [hasSavedOrder, setHasSavedOrder] = useState(false);
   const [hasSent, sethasSent] = useState(false);
   const session_id = searchParams.get('session_id');
@@ -25,14 +26,14 @@ const CheckoutSuccess = () => {
   }, [session_id]);
 
   useEffect(() => {
-    if (data?.status === "complete" && status === "authenticated" && hasSavedOrder) {
+    if (data && 'status' in data && data.status === 'complete' && status === "authenticated" && hasSavedOrder) {
       setCartItems([]);
       emptyCart();
     }
   }, [status, data, hasSavedOrder]);
 
   useEffect(() => {
-    if (data?.status === "complete" && !hasSavedOrder) {
+    if (data && 'status' in data && data.status === 'complete' && !hasSavedOrder) {
       saveOrder(data, setHasSavedOrder, cartItems);
 
       sendEmail(data, sethasSent);
@@ -43,11 +44,11 @@ const CheckoutSuccess = () => {
     document.title = "Purchase Result | Ecommerce Template"
   }, [])
 
-  const fetchCheckoutData = async (url) => {
+  const fetchCheckoutData = async (url: string) => {
     try {
       const responseData = await fetch(url).then((res) => res.json());
       setData(responseData);
-    } catch (err) {
+    } catch (err: any) {
       setData({
         error: true,
         errorMessage: "There was an error getting the data from the server.",
@@ -58,10 +59,10 @@ const CheckoutSuccess = () => {
 
   const emptyCart = async () => {
     try {
-      await axios.put(`/api/cart?userId=${session.user._id}`, {
+      await axios.put(`/api/cart?userId=${session?.user._id}`, {
         cart: [],
       });
-    } catch (error) {
+    } catch (error: any) {
       throw new Error(error.message);
     }
   };
@@ -69,13 +70,15 @@ const CheckoutSuccess = () => {
   return (
     <section className="pt-12">
       <div className="flex flex-col gap-2">
-        {data && data.error ?
+        {data && 'error' in data ?
           <p>{data.errorMessage}</p>
           : data && hasSavedOrder && hasSent ?
             <>
-              <h1 className='text-xl sm:text-2xl font-bold mb-3'>Checkout Payment Result</h1>
-              <h3 className='text-lg	font-semibold'>Successful payment</h3>
-              <p>{`An email has been sent to you at: ${data.customer_details.email}`}</p>
+              <h1 className='mb-3 text-xl font-bold sm:text-2xl'>Checkout Payment Result</h1>
+              <h3 className='text-lg font-semibold'>Successful payment</h3>
+              {data.customer_details && (
+                <p>{`An email has been sent to you at: ${data.customer_details.email}`}</p>
+              )}
             </>
             :
             <Loader />
