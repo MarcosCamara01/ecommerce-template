b30d04475767563877dc5363e66ce6f97e4e01c9
Marcos Penelas <marcospenelascamara@gmail.com>
2024-03-17 00:01:16 +0000

    svg instead react icons

--- src/components/account/Signup.tsx
+++ src/components/account/Signup.tsx
@@ -4,9 +4,6 @@ import { FormEvent, useEffect, useState } from "react";
 import axios, { AxiosError } from "axios";
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
-import { BiLogoGoogle } from 'react-icons/bi';
-import { BiSolidShow } from 'react-icons/bi';
-import { BiSolidHide } from 'react-icons/bi';
 
 const Signup = () => {
     const labelStyles = "w-full text-sm";
@@ -31,7 +28,7 @@ const Signup = () => {
                 phone: formData.get("phone"),
             });
 
-            const res = await signIn("credentials", {
+            await signIn("credentials", {
                 email: signupResponse.data.email,
                 password: formData.get("password"),
                 redirect: false,
@@ -83,7 +80,37 @@ const Signup = () => {
                             setShowPassword(!showPassword)
                         }}
                     >
-                        {showPassword ? <BiSolidHide /> : <BiSolidShow />}
+                        {showPassword
+                            ? <svg
+                                data-testid="geist-icon"
+                                height="16"
+                                strokeLinejoin="round"
+                                viewBox="0 0 16 16"
+                                width="16"
+                                style={{ color: 'currentColor' }}
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    d="M4.02168 4.76932C6.11619 2.33698 9.88374 2.33698 11.9783 4.76932L14.7602 7.99999L11.9783 11.2307C9.88374 13.663 6.1162 13.663 4.02168 11.2307L1.23971 7.99999L4.02168 4.76932ZM13.1149 3.79054C10.422 0.663244 5.57797 0.663247 2.88503 3.79054L-0.318359 7.5106V8.48938L2.88503 12.2094C5.57797 15.3367 10.422 15.3367 13.1149 12.2094L16.3183 8.48938V7.5106L13.1149 3.79054ZM6.49997 7.99999C6.49997 7.17157 7.17154 6.49999 7.99997 6.49999C8.82839 6.49999 9.49997 7.17157 9.49997 7.99999C9.49997 8.82842 8.82839 9.49999 7.99997 9.49999C7.17154 9.49999 6.49997 8.82842 6.49997 7.99999ZM7.99997 4.99999C6.34311 4.99999 4.99997 6.34314 4.99997 7.99999C4.99997 9.65685 6.34311 11 7.99997 11C9.65682 11 11 9.65685 11 7.99999C11 6.34314 9.65682 4.99999 7.99997 4.99999Z"
+                                    fill="currentColor"
+                                ></path>
+                            </svg>
+                            : <svg
+                                data-testid="geist-icon"
+                                height="16"
+                                strokeLinejoin="round"
+                                viewBox="0 0 16 16"
+                                width="16"
+                                style={{ color: 'currentColor' }}
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    d="M0.191137 2.06228L0.751694 2.56055L14.2517 14.5605L14.8122 15.0588L15.8088 13.9377L15.2482 13.4394L13.4399 11.832L16.3183 8.48938V7.51059L13.1149 3.79053C10.6442 0.921301 6.36413 0.684726 3.59378 3.07992L1.74824 1.43943L1.18768 0.941162L0.191137 2.06228ZM14.7602 7.99998L12.3187 10.8354L10.6699 9.36978C11.249 8.24171 11.0661 6.82347 10.1213 5.87865C9.08954 4.8469 7.49326 4.72376 6.32676 5.50923L4.72751 4.08767C6.88288 2.36327 10.1023 2.59076 11.9783 4.76931L14.7602 7.99998ZM7.52702 6.57613L9.46929 8.30259C9.56713 7.82531 9.43091 7.30959 9.06063 6.93931C8.64578 6.52446 8.0484 6.4034 7.52702 6.57613ZM-0.318359 7.51059L1.40386 5.5106L2.54051 6.48938L1.23971 7.99998L4.02168 11.2307C5.52853 12.9805 7.90301 13.4734 9.89972 12.7017L10.4405 14.1008C7.88008 15.0904 4.82516 14.4625 2.88503 12.2094L-0.318359 8.48938V7.51059Z"
+                                    fill="currentColor"
+                                ></path>
+                            </svg>}
                     </button>
                 </div>
 
@@ -108,7 +135,32 @@ const Signup = () => {
                 <button
                     className="flex items-center gap-3 px-4 py-2 text-sm align-middle transition duration-150 bg-black border border-solid rounded text-999 border-border-primary ease hover:bg-color-secondary"
                     onClick={() => signIn("google")}>
-                    <BiLogoGoogle className="text-2xl" /> Sign in with Google
+                    <svg
+                        data-testid="geist-icon"
+                        height="20"
+                        strokeLinejoin="round"
+                        viewBox="0 0 16 16"
+                        width="20"
+                        style={{ color: 'currentColor' }}
+                    >
+                        <path
+                            d="M8.15991 6.54543V9.64362H12.4654C12.2763 10.64 11.709 11.4837 10.8581 12.0509L13.4544 14.0655C14.9671 12.6692 15.8399 10.6182 15.8399 8.18188C15.8399 7.61461 15.789 7.06911 15.6944 6.54552L8.15991 6.54543Z"
+                            fill="#4285F4"
+                        ></path>
+                        <path
+                            d="M3.6764 9.52268L3.09083 9.97093L1.01807 11.5855C2.33443 14.1963 5.03241 16 8.15966 16C10.3196 16 12.1305 15.2873 13.4542 14.0655L10.8578 12.0509C10.1451 12.5309 9.23598 12.8219 8.15966 12.8219C6.07967 12.8219 4.31245 11.4182 3.67967 9.5273L3.6764 9.52268Z"
+                            fill="#34A853"
+                        ></path>
+                        <path
+                            d="M1.01803 4.41455C0.472607 5.49087 0.159912 6.70543 0.159912 7.99995C0.159912 9.29447 0.472607 10.509 1.01803 11.5854C1.01803 11.5926 3.6799 9.51991 3.6799 9.51991C3.5199 9.03991 3.42532 8.53085 3.42532 7.99987C3.42532 7.46889 3.5199 6.95983 3.6799 6.47983L1.01803 4.41455Z"
+                            fill="#FBBC05"
+                        ></path>
+                        <path
+                            d="M8.15982 3.18545C9.33802 3.18545 10.3853 3.59271 11.2216 4.37818L13.5125 2.0873C12.1234 0.792777 10.3199 0 8.15982 0C5.03257 0 2.33443 1.79636 1.01807 4.41455L3.67985 6.48001C4.31254 4.58908 6.07983 3.18545 8.15982 3.18545Z"
+                            fill="#EA4335"
+                        ></path>
+                    </svg>
+                    Sign in with Google
                 </button>
                 <Link href="/login" className="text-sm transition duration-150 text-color-tertiary ease hover:text-white">Already have an account?</Link>
             </form>
--- src/components/account/UpdateButton.jsx
+++ src/components/account/UpdateButton.jsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
-import { IoIosArrowForward } from 'react-icons/io';
 import axios from 'axios';
 import { useSession } from 'next-auth/react';
 import { toast } from 'sonner';
@@ -47,7 +46,6 @@ export const UpdateButton = ({ text, isMobile }) => {
     const cellButtonStyles = "flex justify-between items-center rounded p-2.5 transition duration-150 ease hover:bg-color-secondary";
     const cellLeftStyles = "flex justify-between flex-col items-start gap-1 text-sm";
     const spanLeftStyles = "text-color-tertiary text-13 mt-1";
-    const svgStyles = "text-xl";
 
     return (
         <>
@@ -65,7 +63,21 @@ export const UpdateButton = ({ text, isMobile }) => {
                         : session?.user[text]}</span>
                 </div>
                 <div>
-                    <IoIosArrowForward className={svgStyles} />
+                    <svg
+                        data-testid="geist-icon"
+                        height="16"
+                        strokeLinejoin="round"
+                        viewBox="0 0 16 16"
+                        width="16"
+                        style={{ color: 'currentColor' }}
+                    >
+                        <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M5.50001 1.93933L6.03034 2.46966L10.8536 7.29288C11.2441 7.68341 11.2441 8.31657 10.8536 8.7071L6.03034 13.5303L5.50001 14.0607L4.43935 13L4.96968 12.4697L9.43935 7.99999L4.96968 3.53032L4.43935 2.99999L5.50001 1.93933Z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
                 </div>
             </button>
 
