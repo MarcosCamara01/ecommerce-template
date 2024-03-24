7ba01cf5d8bc14464edb610a86723a52e7fc6c45
Marcos Penelas <marcospenelascamara@gmail.com>
2024-03-24 19:53:24 +0000

    images sizes

--- src/components/products/ProductImages.tsx
+++ src/components/products/ProductImages.tsx
@@ -94,6 +94,10 @@ export const Images = (
         priority={priority}
         className="w-full max-w-img aspect-[2/3]"
         onLoad={handleImageLoadComplete}
+        sizes="(max-width: 640px) 100vw,
+          (max-width: 1154px) 33vw,
+          (max-width: 1536px) 25vw,
+          20vw"
       />
       <div className={!imageLoaded ? 'absolute top-0 right-0 w-full aspect-[2/3] bg-black' : 'hidden'}>
         <Skeleton className="w-full aspect-[2/3] rounded-b-none" />
