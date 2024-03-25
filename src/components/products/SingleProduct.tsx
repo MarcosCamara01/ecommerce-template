e25ec3dad70c76d039ea6bf4582a8bbde70d2121
Marcos Penelas <marcospenelascamara@gmail.com>
2024-03-25 23:23:01 +0000

    product accordion

--- src/components/products/SingleProduct.tsx
+++ src/components/products/SingleProduct.tsx
@@ -5,6 +5,12 @@ import { ProductDocument, VariantsDocument } from "@/types/types";
 import AddToCart from "../cart/AddToCart";
 import { Session } from "next-auth";
 import { useEffect, useState } from "react";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
 
 interface SingleProduct {
     product: string;
@@ -35,7 +41,7 @@ export const SingleProduct = ({ product, isMobile, session }: SingleProduct) => 
                 />
             </div>
 
-            <div className="sticky flex items-center justify-center w-full h-full grow basis-600 top-8">
+            <div className="sticky flex flex-col items-center justify-center w-full h-full gap-5 grow basis-600 top-8">
                 <div className='w-full border border-solid rounded border-border-primary bg-background-secondary'>
                     <div className="flex flex-col justify-between gap-3 p-5 border-b border-solid border-border-primary" >
                         <h1 className="text-base font-semibold">{productPlainObject.name}</h1>
@@ -49,8 +55,50 @@ export const SingleProduct = ({ product, isMobile, session }: SingleProduct) => 
                         selectedVariant={selectedVariant}
                         setSelectedVariant={setSelectedVariant}
                     />
-
                 </div>
+
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>COMPOSITION</AccordionTrigger>
+                        <AccordionContent>
+                            <p>
+                                We work with monitoring programmes to ensure compliance with our social,
+                                environmental and health and safety standards for our products.
+                                To assess compliance, we have developed a programme of audits and continuous
+                                improvement plans.
+                            </p>
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>CARE</AccordionTrigger>
+                        <AccordionContent className="flex flex-col gap-2">
+                            <p> Caring for your clothes is caring for the environment.</p>
+                            <p>
+                                Lower temperature washes and delicate spin cycles are gentler on
+                                garments and help to protect the colour, shape and structure of the fabric.
+                                Furthermore, they reduce the amount of energy used in care processes.
+                            </p>
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>ORIGIN</AccordionTrigger>
+                        <AccordionContent className="flex flex-col gap-2">
+                            <p>
+                                We work with our suppliers, workers, unions and international organisations
+                                to develop a supply chain in which human rights are respected and promoted,
+                                contributing to the United Nations Sustainable Development Goals.
+                            </p>
+                            <p>
+                                Thanks to the collaboration with our suppliers,
+                                we work to know the facilities and processes used to manufacture
+                                our products in order to understand their traceability.
+                            </p>
+                            <p>
+                                Made in Portugal
+                            </p>
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
             </div>
         </div>
     );
